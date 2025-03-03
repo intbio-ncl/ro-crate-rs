f06b952ead53f93461431c21e4a3d6a25aa957a5
Matt Burridge <81111055+mburridge96@users.noreply.github.com>
2025-03-03 11:22:19 +0000

    updated python docstrings

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -41,9 +41,13 @@ struct PyRoCrateContext {
 /// CrateContext methods
 #[pymethods]
 impl PyRoCrateContext {
-    /// Crates a RoCrate Context from just a string
+    /// Creates a context from a single string reference.
     ///
-    /// Used for creating a base RoCrate vocab
+    /// # Arguments
+    /// * `context` - A string reference to a context URL or term.
+    ///
+    /// # Returns
+    /// A `PyRoCrateContext` containing a reference context.
     #[staticmethod]
     fn from_string(context: &str) -> Self {
         PyRoCrateContext {
@@ -54,6 +58,17 @@ impl PyRoCrateContext {
     /// Creates heterogenous context
     ///
     /// Allows for a Reference, Embedded and Extended RoCrate context.
+    ///
+    /// Arguments
+    /// * `context` - A Python list (`PyList`) whose elements are either:
+    ///     - String references representing context URLs/terms.
+    ///     - Python dictionaries representing embedded context mappings.
+    ///
+    /// # Errors
+    /// Raises a `ValueError` if the list contains elements that are not strings or dicts.
+    ///
+    /// # Returns
+    /// A `PyRoCrateContext` containing an extended context.
     #[staticmethod]
     fn from_list<'py>(context: Bound<'py, PyList>) -> PyResult<Self> {
         let mut context_items = Vec::new();
@@ -83,7 +98,13 @@ impl PyRoCrateContext {
 
 #[pymethods]
 impl PyRoCrate {
-    /// Crates a new empty RoCrate
+    /// Creates a new, empty RO-Crate with the specified context.
+    ///
+    /// # Arguments
+    /// * `context` - A `PyRoCrateContext` specifying how the crate’s context is defined.
+    ///
+    /// # Returns
+    /// A new, empty `PyRoCrate`.
     #[new]
     fn new(context: &PyRoCrateContext) -> Self {
         PyRoCrate {
@@ -94,17 +115,32 @@ impl PyRoCrate {
         }
     }
 
+    /// Creates a new default RO-Crate.
+    ///
+    /// # Returns
+    /// A new `PyRoCrate` populated with default metadata and a root dataset entity.
     #[staticmethod]
     fn new_default() -> Self {
         PyRoCrate::default()
     }
 
+    /// Returns all contexts in the crate
+    ///
+    /// # Returns
+    /// A Python list of context strings.
     fn get_all_context(&mut self, py: Python) -> PyResult<Py<PyList>> {
         let test2 = self.inner.context.get_all_context();
         let py_list = PyList::new(py, test2.iter().map(|s| s.as_str()))?;
         Ok(py_list.into())
     }
 
+    /// Returns a specific context entry for the given key.
+    ///
+    /// # Arguments
+    /// * `context` - A Python string (or other PyObject) specifying the key.
+    ///
+    /// # Returns
+    /// A Python string representing the requested context entry.
     fn get_specific_context(&mut self, py: Python, context: PyObject) -> PyResult<Py<PyString>> {
         let context_str: &str = context.extract(py)?;
         let specific_context = self
@@ -115,12 +151,22 @@ impl PyRoCrate {
         Ok(PyString::new(py, &specific_context).into())
     }
 
+    /// Returns the crate's URN UUID.
+    ///
+    /// # Returns
+    /// A Python string containing the UUID (URN).
     fn get_urn_uuid(&mut self, py: Python) -> PyResult<Py<PyString>> {
         let urn = self.inner.context.get_urn_uuid().unwrap();
         Ok(PyString::new(py, &urn).into())
     }
 
-    /// Gets a specified entity based upon ID
+    /// Retrieves an entity by its ID.
+    ///
+    /// # Arguments
+    /// * `id` - A string representing the entity's identifier.
+    ///
+    /// # Returns
+    /// A Python dictionary representing the entity, if found. Otherwise raises `ValueError`.
     fn get_entity(&mut self, py: Python, id: &str) -> PyResult<PyObject> {
         match self.inner.get_entity(id) {
             Some(GraphVector::DataEntity(data_entity)) => {
@@ -147,6 +193,11 @@ impl PyRoCrate {
     /// Lazy update of data entity, finds id and overwrites the index.
     /// Strongly recommended to extract index data, modify, then rewrite the
     /// modified index data as the update.
+    /// # Arguments
+    /// * `py_obj` - A Python object representing a DataEntity (dictionary-like).
+    ///
+    /// # Returns
+    /// `Ok(())` on success; an error otherwise.
     fn update_data(&mut self, py: Python, py_obj: PyObject) -> PyResult<()> {
         // Needs to check if data entity first - then parse as contextual if fail
         // if data then append to partOf vec in root.
@@ -168,6 +219,11 @@ impl PyRoCrate {
     /// Lazy update of contextual entity, finds id and overwrites the index.
     /// Strongly recommended to extract index data, modify, then rewrite the
     /// modified index data as the update.
+    /// # Arguments
+    /// * `py_obj` - A Python object representing a ContextualEntity (dictionary-like).
+    ///
+    /// # Returns
+    /// `Ok(())` on success; an error otherwise.
     fn update_contextual(&mut self, py: Python, py_obj: PyObject) -> PyResult<()> {
         // Needs to check if data entity first - then parse as contextual if fail
         // if data then append to partOf vec in root.
@@ -189,6 +245,11 @@ impl PyRoCrate {
     /// Lazy update of root entity, finds id and overwrites the index.
     /// Strongly recommended to extract index data, modify, then rewrite the
     /// modified index data as the update.
+    /// # Arguments
+    /// * `py_obj` - A Python object representing a RootDataEntity (dictionary-like).
+    ///
+    /// # Returns
+    /// `Ok(())` on success; an error otherwise.
     fn update_root(&mut self, py: Python, py_obj: PyObject) -> PyResult<()> {
         // Needs to check if data entity first - then parse as contextual if fail
         // if data then append to partOf vec in root.
@@ -210,6 +271,11 @@ impl PyRoCrate {
     /// Lazy update of metadata descriptor, finds id  and overwrites the index.
     /// Strongly recommended to extract index data, modify, then rewrite the
     /// modified index data as the update.
+    /// # Arguments
+    /// * `py_obj` - A Python object representing a MetadataDescriptor (dictionary-like).
+    ///
+    /// # Returns
+    /// `Ok(())` on success; an error otherwise.
     fn update_descriptor(&mut self, py: Python, py_obj: PyObject) -> PyResult<()> {
         // Needs to check if data entity first - then parse as contextual if fail
         // if data then append to partOf vec in root.
@@ -229,18 +295,35 @@ impl PyRoCrate {
     ///
     /// Overvwrites an ID with a New ID, and recursively changes every instance
     /// of the old ID within the RO-Crate.
+    /// # Arguments
+    /// * `id_old` - The old identifier string.
+    /// * `id_new` - The new identifier string.
+    ///
+    /// # Returns
+    /// `Ok(())` on success.
     fn replace_id(&mut self, id_old: &str, id_new: &str) -> PyResult<()> {
         self.inner.update_id_recursive(id_old, id_new);
         Ok(())
     }
 
     /// Entity deletion both recursive and not
+    /// # Arguments
+    /// * `id` - The entity identifier to remove.
+    /// * `recursive` - A boolean indicating whether related references should also be removed.
+    ///
+    /// # Returns
+    /// `Ok(())` on success.
     fn delete_entity(&mut self, id: &str, recursive: bool) -> PyResult<()> {
         self.inner.remove_by_id(id, recursive);
         Ok(())
     }
 
     /// Writes ro-crate back to ro-crate-metadata.json
+    /// # Arguments
+    /// * `file_path` - An optional string path to write the crate JSON.
+    ///
+    /// # Returns
+    /// `Ok(())` on success; an error otherwise.
     fn write(&self, file_path: Option<String>) -> PyResult<()> {
         let path = file_path.unwrap_or_else(|| "ro-crate-metadata.json".to_string());
         rs_write_crate(&self.inner, path);
@@ -248,8 +331,10 @@ impl PyRoCrate {
     }
 
     /// Print's full crate
+    /// # Returns
+    /// A string showing the internal state of the `PyRoCrate`.
     fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("PyRoCrate(data: '{:#?}')", self.inner))
+        Ok(format!("PyRoCrate(data: '{:#?}')", self))
     }
 }
 
@@ -261,6 +346,16 @@ impl From<RoCrate> for PyRoCrate {
 }
 
 /// Reads a crate into memory allowing manipulation
+/// # Arguments
+/// * `relative_path` - Path (string) to an existing RO-Crate JSON or folder.
+/// * `validation_level` - An integer specifying how strictly the crate is validated.
+///                        0-2, 0 no validation, 1 warnings, 2 strict
+///
+/// # Errors
+/// Raises an `IOError` if it cannot read or parse the crate.
+///
+/// # Returns
+/// A `PyRoCrate` on success.
 #[pyfunction]
 fn read(relative_path: &str, validation_level: i8) -> PyResult<PyRoCrate> {
     let path = Path::new(relative_path).to_path_buf();
@@ -272,6 +367,17 @@ fn read(relative_path: &str, validation_level: i8) -> PyResult<PyRoCrate> {
 /// Reads a json object of a crate into memory allowing manipulation
 ///
 /// Useful for json from browsers/ applications
+/// # Arguments
+/// * `obj` - JSON string object of RO-Crate JSON.
+/// * `validation_level` - An integer specifying how strictly the crate is validated.
+///                        0-2, 0 no validation, 1 warnings, 2 strict
+///
+/// # Errors
+/// Raises an `IOError` if it cannot read or parse the crate.
+///
+/// # Returns
+/// A `PyRoCrate` on success.
+
 #[pyfunction]
 fn read_object(obj: &str, validation_level: i8) -> PyResult<PyRoCrate> {
     let rocrate = read_crate_obj(obj, validation_level)
@@ -281,14 +387,30 @@ fn read_object(obj: &str, validation_level: i8) -> PyResult<PyRoCrate> {
 /// Reads a Zip of a crate into memory allowing manipulation
 ///
 /// Useful for understanding large archives
+/// # Arguments
+/// * `path` - Path (string) to an existing RO-Crate ZIP.
+/// * `validation_level` - An integer specifying how strictly the crate is validated.
+///                        0-2, 0 no validation, 1 warnings, 2 strict
+///
+/// # Errors
+/// Raises an `IOError` if it cannot read or parse the crate.
+///
+/// # Returns
+/// A `PyRoCrate` on success.
 #[pyfunction]
-fn read_zip(obj: &str, validation_level: i8) -> PyResult<PyRoCrate> {
-    let rocrate = parse_zip(obj, validation_level)
+fn read_zip(path: &str, validation_level: i8) -> PyResult<PyRoCrate> {
+    let rocrate = parse_zip(path, validation_level)
         .map_err(|e| PyIOError::new_err(format!("Failed to read crate: {:#?}", e)))?;
     Ok(PyRoCrate::from(rocrate))
 }
 
-// Writes to parquet
+/// Writes to parquet
+/// # Arguments
+/// * `rocrate` - The `PyRoCrate` to be written.
+/// * `path` - The output path to the Parquet file.
+///
+/// # Returns
+/// No explicit return; writes the file or raises an error.
 #[pyfunction]
 fn write_parquet(rocrate: &PyRoCrate, path: &str) {
     let mut df = to_df(&rocrate.inner);
@@ -297,6 +419,14 @@ fn write_parquet(rocrate: &PyRoCrate, path: &str) {
     wr_par(&mut df, target_path);
 }
 
+/// Enables objects URLs to be prefixed against ro-crate location to rewrite for
+/// object storage
+/// # Arguments
+/// * `rocrate` - The `PyRoCrate` whose entity IDs should be updated.
+/// * `object_root` - A string prefix (e.g., S3/GCS bucket URL) to prepend to entity paths.
+///
+/// # Returns
+/// No explicit return; mutates the `PyRoCrate` in-place.
 #[pyfunction]
 fn prefix_object_id(rocrate: &mut PyRoCrate, object_root: &str) {
     relative_to_object_store(&mut rocrate.inner, object_root);
@@ -304,6 +434,16 @@ fn prefix_object_id(rocrate: &mut PyRoCrate, object_root: &str) {
 
 /// Targets a ro-crate and zips directory contents
 /// Flatten removes directory structure in Zip
+/// # Arguments
+/// * `crate_path` - The path to the RO-Crate directory or JSON file.
+/// * `external` - Whether to include external files (outside the directory) in the ZIP.
+/// * `validation_level` - The validation strictness level (0-2, 0 no validation, 1 warning, 2
+/// strict).
+/// * `flatten` - If true, flattens the directory structure inside the ZIP.
+/// * `unique` - If true, ensures unique file names within the ZIP.
+///
+/// # Returns
+/// `Ok(())` on success; raises an error otherwise.
 #[pyfunction]
 fn zip(
     crate_path: &str,

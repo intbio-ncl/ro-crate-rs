5a3a2ded59ca1b1700c3303b091a9d90c1836fde
Sebastian Beyvers <sb@pus.de>
2026-03-26 11:18:34 +0100

    chore: Cargo format imports

--- benches/create_and_write.rs
+++ benches/create_and_write.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 use rocraters::ro_crate::constraints::*;
 use rocraters::ro_crate::contextual_entity::ContextualEntity;
